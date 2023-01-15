d4467da677286ced5a170095e40d4fb4735a0416
andydollin-pc <cervantes21@DESKTOP-Q8PT42K>
2023-01-14 19:49:54 -0600

    Add button for clear

--- paint-v02.py
+++ paint-v02.py
@@ -4,21 +4,30 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Line, Color
 from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 class Dibujador(Widget):
     Window.clearcolor = (1,1,1,1)
  
     def on_touch_down(self, touch):
-        print(touch.pos)
         with self.canvas:
             Color(0,0,0)           
             touch.ud['dibujo'] = Line(points=touch.pos)
     def on_touch_move(self, touch):
         touch.ud['dibujo'].points += touch.pos
- 
+    def borrar(self, *args):
+        self.canvas.clear()
+
 class MiApp(App):
     def build(self):
-        return Dibujador()
+        dibujador = Dibujador()
+        borrar_btn = Button(text='Borrar')
+        borrar_btn.bind(on_release=dibujador.borrar)
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(dibujador)
+        layout.add_widget(borrar_btn)
+        return layout
+
 if __name__ == "__main__":
     MiApp().run()
-
