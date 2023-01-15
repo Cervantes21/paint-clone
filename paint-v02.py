5fc2f7251b50088ce1aabac822b9361b153d8dd2
andy-dollin <cervantes21@DESKTOP-Q8PT42K>
2023-01-14 19:58:23 -0600

    Upgrade button for clear

--- paint-v02.py
+++ paint-v02.py
@@ -5,29 +5,28 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Line, Color
 from kivy.core.window import Window
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
 
 class Dibujador(Widget):
     Window.clearcolor = (1,1,1,1)
- 
+
     def on_touch_down(self, touch):
         with self.canvas:
             Color(0,0,0)           
             touch.ud['dibujo'] = Line(points=touch.pos)
     def on_touch_move(self, touch):
         touch.ud['dibujo'].points += touch.pos
-    def borrar(self, *args):
+    def limpiar_pantalla(self, obj):
         self.canvas.clear()
+
 
 class MiApp(App):
     def build(self):
+        parent = Widget()
         dibujador = Dibujador()
-        borrar_btn = Button(text='Borrar')
-        borrar_btn.bind(on_release=dibujador.borrar)
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(dibujador)
-        layout.add_widget(borrar_btn)
-        return layout
-
+        btn = Button(text='Borrar', size_hint=(None, None), size=(100, 50))
+        btn.bind(on_press=dibujador.limpiar_pantalla)
+        parent.add_widget(dibujador)
+        parent.add_widget(btn)
+        return parent
 if __name__ == "__main__":
     MiApp().run()
