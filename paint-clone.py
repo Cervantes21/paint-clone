91e3f5fac5a04ef930b7cf43fb327b0e34183b1d
andy-dollin <cervantes21@DESKTOP-Q8PT42K>
2023-01-14 21:28:25 -0600

    button clear

--- paint-clone.py
+++ paint-clone.py
@@ -5,40 +5,28 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Line, Color
 from kivy.core.window import Window
 from kivy.uix.button import Button
-from kivy.uix.colorpicker import ColorPicker
-from kivy.uix.gridlayout import GridLayout
 
 class Dibujador(Widget):
     Window.clearcolor = (1,1,1,1)
-    color = [0,0,0,1]
+
     def on_touch_down(self, touch):
         with self.canvas:
-            Color(*self.color)
-            touch.ud['dibujo'] = Line(points=(touch.x, touch.y))
-            
+            Color(0,0,0)           
+            touch.ud['dibujo'] = Line(points=touch.pos)
     def on_touch_move(self, touch):
-        touch.ud['dibujo'].points += [touch.x, touch.y]
-
-    def clear_canvas(self):
+        touch.ud['dibujo'].points += touch.pos
+    def limpiar_pantalla(self, obj):
         self.canvas.clear()
 
-    def update_color(self, colorpicker, color):
-        self.color = color
 
 class MiApp(App):
     def build(self):
-        layout = GridLayout(cols=2)
+        parent = Widget()
         dibujador = Dibujador()
-        clear_button = Button(text="Borrar", on_press=dibujador.clear_canvas)
-        color_picker = ColorPicker()
-        color_picker.bind(color=dibujador.update_color)
-        layout.add_widget(dibujador)
-        layout.add_widget(clear_button)
-        layout.add_widget(color_picker)
-
-        return layout
-
+        btn = Button(text='Borrar', size_hint=(None, None), size=(100, 50))
+        btn.bind(on_press=dibujador.limpiar_pantalla)
+        parent.add_widget(dibujador)
+        parent.add_widget(btn)
+        return parent
 if __name__ == "__main__":
     MiApp().run()
-
-
